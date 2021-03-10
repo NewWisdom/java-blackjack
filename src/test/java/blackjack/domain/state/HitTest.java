57b7e737f1d86ab53194957863f1b64749385bfa
NewWisdom <wlgp2500@gmail.com>
2021-03-10 18:37:06 +0900

    test(hit): 상태를 확인하고 draw하는 테스트 추가

---
+++ src/test/java/blackjack/domain/state/HitTest.java
@@ -0,0 +1,46 @@
+package blackjack.domain.state;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Denomination;
+import blackjack.domain.card.Suit;
+import blackjack.domain.user.Cards;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HitTest {
+    @Test
+    @DisplayName("hit 객체를 만든다.")
+    void createHit() {
+        Hit hit = new Hit(new Cards(
+                new Card(Suit.CLOVER, Denomination.ACE),
+                new Card(Suit.HEART, Denomination.ACE)));
+
+        assertThat(hit).isInstanceOf(Hit.class);
+    }
+
+    @Test
+    @DisplayName("상태를 확인하고 draw 한다.")
+    void hitDraw1() {
+        Hit hit = new Hit(new Cards(
+                new Card(Suit.CLOVER, Denomination.JACK),
+                new Card(Suit.HEART, Denomination.TEN)));
+
+        State state = hit.draw(new Card(Suit.CLOVER, Denomination.EIGHT));
+
+        assertThat(state).isInstanceOf(Bust.class);
+    }
+
+    @Test
+    @DisplayName("상태를 확인하고 draw 한다.")
+    void hitDraw2() {
+        Hit hit = new Hit(new Cards(
+                new Card(Suit.CLOVER, Denomination.ACE),
+                new Card(Suit.HEART, Denomination.ACE)));
+
+        State state = hit.draw(new Card(Suit.CLOVER, Denomination.EIGHT));
+
+        assertThat(state).isInstanceOf(Hit.class);
+    }
+}
